5221073d74911a1f2806cdcaad475f1477f83c47
Justin <whoamg@gmail.com>
2024-09-15 20:45:30 +0900

    back to working

--- Incantation/Assets/Scripts/Sim 3D/Compute/SpatialHash3D.hlsl
+++ Incantation/Assets/Scripts/Sim 3D/Compute/SpatialHash3D.hlsl
@@ -36,27 +36,17 @@ static const uint hashK1 = 15823;
 static const uint hashK2 = 9737333;
 static const uint hashK3 = 440817757;
 
-bool isCellInBounds(int3 cell, int3 boundsSize)
-{
-	bool outOfBounds = (cell.x < 0) || (cell.x >= boundsSize.x) ||
-		(cell.y < 0) || (cell.y >= boundsSize.y)
-		|| (cell.z < 0) || (cell.z >= boundsSize.z);
-	return !outOfBounds;
-}
-
 // Convert floating point position into an unsigned grid cell coordinate
-// NOTE: Could potentially return positions out of bounds. Consumer is responsible for checking.
-int3 BoundedGetCell3D(float3 position, float radius, uint3 boundsSize)
+uint3 BoundedGetCell3D(float3 position, float radius, uint3 boundsSize)
 {
-	float3 halfBoundsSizeWorldCoord = 0.5f * boundsSize * radius;
+	float3 halfBoundsSizeWorldCoord = (boundsSize / 2.0f) * radius;
 	float3 offsetPosition = position + halfBoundsSizeWorldCoord;
-	return (int3)floor(offsetPosition / radius);
+	return (uint3)floor(offsetPosition / radius);
 }
 
-// NOTE: Returns keys out of bounds. Consumer is responsible for checking.
-int BoundedKeyCell3D(int3 cell, int3 boundsSize)
+uint BoundedKeyCell3D(uint3 cell, uint3 boundsSize)
 {
-	int key = (boundsSize.x * boundsSize.y * cell.z) + (boundsSize.x * cell.y) + cell.x;
+	uint key = (boundsSize.x * boundsSize.y * cell.z) + (boundsSize.x * cell.y) + cell.x;
 	return key;
 }
 
