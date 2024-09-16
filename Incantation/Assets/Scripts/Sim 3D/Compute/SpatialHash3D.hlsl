af0465f6d7320111063d2b36b02cc41254bac893
Justin <whoamg@gmail.com>
2024-09-16 12:15:47 +0900

    Fixed stupid bubbling problem

--- Incantation/Assets/Scripts/Sim 3D/Compute/SpatialHash3D.hlsl
+++ Incantation/Assets/Scripts/Sim 3D/Compute/SpatialHash3D.hlsl
@@ -36,12 +36,21 @@ static const uint hashK1 = 15823;
 static const uint hashK2 = 9737333;
 static const uint hashK3 = 440817757;
 
+bool isCellInBounds(int3 cell, uint3 boundsSize)
+{
+	return (cell.x >= 0) && (cell.x < boundsSize.x) &&
+		(cell.y >= 0) && (cell.y < boundsSize.y) &&
+		(cell.z >= 0) && (cell.z < boundsSize.z);
+}
+
 // Convert floating point position into an unsigned grid cell coordinate
 uint3 BoundedGetCell3D(float3 position, float radius, uint3 boundsSize)
 {
 	float3 halfBoundsSizeWorldCoord = (boundsSize / 2.0f) * radius;
 	float3 offsetPosition = position + halfBoundsSizeWorldCoord;
-	return (uint3)floor(offsetPosition / radius);
+	float3 unclampedCell = floor(offsetPosition / radius);
+	float3 clampedCell = clamp(unclampedCell, 0, boundsSize - 1);
+	return (uint3)clampedCell;
 }
 
 uint BoundedKeyCell3D(uint3 cell, uint3 boundsSize)
